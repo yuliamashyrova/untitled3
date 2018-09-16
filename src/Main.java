f4541c81b6c62faf582cc06df8d15bad29a9c4ac
Юлия <yulia-mashyrova@mail.ru>
2018-09-16 11:21:29 +0300

    5

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main ()
     {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
 
     }
 }
